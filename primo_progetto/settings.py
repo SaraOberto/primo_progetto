9a6d9d7e16b5b4804647a1f4ff113f2280269f7e
SaraOberto <117723108+SaraOberto@users.noreply.github.com>
2023-10-05 12:41:26 +0200

    aggiornato parametro allowed host

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!3xwgasvfomst=@4214+bnzo0iw@awxk-+(wb2s186_op2!)x6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
