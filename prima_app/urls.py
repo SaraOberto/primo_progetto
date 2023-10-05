aaae3a2c0827cb2e4fe48d1004428fd4e8932e93
SaraOberto <117723108+SaraOberto@users.noreply.github.com>
2023-10-05 11:13:37 +0200

    Initial commit

---
+++ prima_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from prima_app.views import *
+
+
+app_name = "prima_app"
+urlpatterns = [
+    path('', homepage, name = 'homepage'),
+    path('welcome', welcome, name = 'welcome'),
+    path('lista', lista, name = 'lista'),
+    path('chisiamo', chisiamo, name = 'chisiamo'),
+]
