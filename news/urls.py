6f2595db0648793f4bedfdafdb36e74dbbe44dc7
SaraOberto <117723108+SaraOberto@users.noreply.github.com>
2023-11-16 23:09:14 +0100

    passo 17

--- news/urls.py
+++ news/urls.py
@@ -6,4 +6,7 @@ app_name = 'news'
 urlpatterns = [
     path('', home, name="homeview"),
     path("articoli/<int:pk>", articoloDetailView, name="articolo_detail"),
+    path("lista_articoli/<int:pk>", lista_articoli_giornalisti, name="lista_articoli"),
+    path("lista_articoli", lista_articoli_giornalisti, name="lista_articoli"),
+    path("query", query_base, name="query"),
 ]
--- news/views.py
+++ news/views.py
@@ -30,3 +30,83 @@ def articoloDetailView(request, pk):
     #print (response)
     
     #return HttpResponse("<h1>" + response + "</h1>")
+
+def lista_articoli_giornalisti(request, pk):
+    if(pk==None):
+        articoli=Articolo.objects.all()
+    else:
+        articoli=Articolo.objects.filter(giornalista_id=pk)
+    context = {
+        'articoli': articoli,
+        'pk':pk,
+    }
+    return render(request, 'lista_articoli.html', context)
+
+def query_base(request):
+    #1. Tutti gli articoli scritti dai gionalisti di un certo cognome:
+    articoli_cognome = Articolo.objects.filter(giornalista_cognome='Rossi')
+    
+    #2. totale
+    numero_totale_articoli = Articolo.objects.count()
+
+    #3. contare il numero di articoli scritti da un gionalista specifico:
+    giornalista_1 = Giornalista.objects.get(id=1)
+    numero_articoli_giornalista_1 = Articolo.objects.filter(gionalista=giornalista_1).count()
+
+    #4. ordinare gli articoli per numero di visualizzazioni in ordine descrescente:
+    articoli_ordinati = Articolo.objects.order_by('-visualizzazioni')
+
+    #5. tutti gli articoli che non hanno visualizzazioni:
+    articoli_senza_ordinati = Articolo.objects.filter(visualizzazioni=0)
+
+    #6. articolo più visualizzato
+    articolo_piu_visualizzato = Articolo.objects.order_by('-visualizzazioni').first()
+
+    #7. tutti i giornalisti nati dopo una certa data:
+    gionalisti_data= Giornalista.objects.filter(anno_di_nascita__gt=datetime.date(1900, 1, 1))
+
+    #8. tutti gli articoli pubblicati in una data specifica:
+    articoli_del_giorno=Articolo.objects.filter(date=datetime.date(2023, 1, 1))
+
+    #9 tutti gli articoli pubblicati in un intervallo di date:
+    articoli_periodo= Articolo.objects.filter(data__range=(datetime.date(2023, 1, 1), datetime.date(2023, 12, 31)))
+
+    #10 gli articoli scritti da gionalisti nati prima del 1980:
+    gionalisti_nati= Giornalista.objects.filter(anno_di_nascita__lt=datetime.date(1980, 1, 1))
+    articoli_gionalisti= Articolo.objects.filter(gionalista__in=giornalista_nati)
+
+    #11 il gionalista più giovane:
+    gionalista_giovane=Giornalista.objects.order_by('anno_di_nascita').first()
+
+    #12 il gionalista più anziano:
+    gionalista_anziano=Giornalista.objects.order_by('-anno_di_nascita').first()
+
+    #13 gli ultmi 5 articoli pubblicati:
+    ultimi= Articolo.objects.order_by('-data')[:5]
+
+    #14 tutti gli articoli con un certo numero minimo di visualizzazioni:
+    articoli_minime_visualizzazioni= Articolo.objects.filter(visualizzazioni__gte=100)
+
+     #15 tutti gli articoli che contengono una certa parola nel titolo:
+    articoli_parola= Articolo.objects.filter(titolo__icontains='importance')
+
+    #creare il dizionario context
+    context= {
+        'articoli_cognome': articoli_cognome,
+        'numero_totale_articoli': numero_totale_articoli,
+        'giornalista_1': giornalista_1,
+        'numero_articoli_giornalista_1': numero_articoli_giornalista_1,
+        'articoli_ordinati': articoli_ordinati,
+        'articoli_senza_ordinati': articoli_senza_ordinati,
+        'articolo_piu_visualizzato': articolo_piu_visualizzato,
+        'gionalisti_data': gionalisti_data,
+        'articoli_del_giorno': articoli_del_giorno,
+        'articoli_periodo': articoli_periodo,
+        'gionalista_giovane': gionalista_giovane,
+        'gionalista_anziano': gionalista_anziano,
+        'ultimi': ultimi,
+        'articoli_minime_visualizzazioni': articoli_minime_visualizzazioni,
+        'articoli_parola': articoli_parola,
+    }
+
+    return render (request, 'query.html', context)
