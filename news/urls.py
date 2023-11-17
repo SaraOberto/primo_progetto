9922ea5784c9485802f99ea6323b4aca3a38de1e
SaraOberto <117723108+SaraOberto@users.noreply.github.com>
2023-11-17 09:50:35 +0100

    passo 18

--- news/urls.py
+++ news/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import home, articoloDetailView
+from .views import *
 
 app_name = 'news'
 
 urlpatterns = [
-    path('', home, name="homeview"),
+    path('homepage_news/', homepage_news, name="homepage_news"),
     path("articoli/<int:pk>", articoloDetailView, name="articolo_detail"),
+    path("articoli/", articoloDetailView, name="articolo_detail"),
     path("lista_articoli/<int:pk>", lista_articoli_giornalisti, name="lista_articoli"),
-    path("lista_articoli", lista_articoli_giornalisti, name="lista_articoli"),
-    path("query", query_base, name="query"),
+    path("lista_articoli/", lista_articoli_giornalisti, name="lista_articoli"),
+    path("query_base/", query_base, name="query_base"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Articolo, Giornalista
+import datetime
 # Create your views here.
 
-def home(request):
+def homepage_news(request):
     a = ""
     g = ""
     articoli = Articolo.objects.all()
     giornalisti = Giornalista.objects.all()
     context = {"articoli": articoli, "giornalisti": giornalisti}
     print(context)
-    return render(request, "homepage.html", context)
+    return render(request, "homepage_news.html", context)
 
 def articoloDetailView(request, pk):
     #articolo = Articolo.objects.get(pk=pk)
@@ -31,7 +32,7 @@ def articoloDetailView(request, pk):
     
     #return HttpResponse("<h1>" + response + "</h1>")
 
-def lista_articoli_giornalisti(request, pk):
+def lista_articoli_giornalisti(request, pk=None):
     if(pk==None):
         articoli=Articolo.objects.all()
     else:
@@ -44,42 +45,42 @@ def lista_articoli_giornalisti(request, pk):
 
 def query_base(request):
     #1. Tutti gli articoli scritti dai gionalisti di un certo cognome:
-    articoli_cognome = Articolo.objects.filter(giornalista_cognome='Rossi')
+    articoli_cognome = Articolo.objects.filter(giornalista__cognome='Marinelli')
     
     #2. totale
     numero_totale_articoli = Articolo.objects.count()
 
     #3. contare il numero di articoli scritti da un gionalista specifico:
-    giornalista_1 = Giornalista.objects.get(id=1)
-    numero_articoli_giornalista_1 = Articolo.objects.filter(gionalista=giornalista_1).count()
+    giornalista_1 = Giornalista.objects.get(id=4)
+    numero_articoli_giornalista_1 = Articolo.objects.filter(giornalista=giornalista_1).count()
 
     #4. ordinare gli articoli per numero di visualizzazioni in ordine descrescente:
     articoli_ordinati = Articolo.objects.order_by('-visualizzazioni')
 
     #5. tutti gli articoli che non hanno visualizzazioni:
-    articoli_senza_ordinati = Articolo.objects.filter(visualizzazioni=0)
+    articoli_senza_visualizzazioni = Articolo.objects.filter(visualizzazioni=0)
 
     #6. articolo più visualizzato
     articolo_piu_visualizzato = Articolo.objects.order_by('-visualizzazioni').first()
 
     #7. tutti i giornalisti nati dopo una certa data:
-    gionalisti_data= Giornalista.objects.filter(anno_di_nascita__gt=datetime.date(1900, 1, 1))
+    giornalista_data= Giornalista.objects.filter(anno_di_nascita__gt=datetime.date(1900, 1, 1))
 
     #8. tutti gli articoli pubblicati in una data specifica:
-    articoli_del_giorno=Articolo.objects.filter(date=datetime.date(2023, 1, 1))
+    articoli_del_giorno=Articolo.objects.filter(data=datetime.date(2023, 1, 1))
 
     #9 tutti gli articoli pubblicati in un intervallo di date:
     articoli_periodo= Articolo.objects.filter(data__range=(datetime.date(2023, 1, 1), datetime.date(2023, 12, 31)))
 
-    #10 gli articoli scritti da gionalisti nati prima del 1980:
-    gionalisti_nati= Giornalista.objects.filter(anno_di_nascita__lt=datetime.date(1980, 1, 1))
-    articoli_gionalisti= Articolo.objects.filter(gionalista__in=giornalista_nati)
+    #10 gli articoli scritti da giornalista nati prima del 1980:
+    giornalista_nati= Giornalista.objects.filter(anno_di_nascita__lt=datetime.date(1980, 1, 1))
+    articoli_giornalista= Articolo.objects.filter(giornalista__in=giornalista_nati)
 
     #11 il gionalista più giovane:
-    gionalista_giovane=Giornalista.objects.order_by('anno_di_nascita').first()
+    giornalista_giovane=Giornalista.objects.order_by('-anno_di_nascita').first()
 
     #12 il gionalista più anziano:
-    gionalista_anziano=Giornalista.objects.order_by('-anno_di_nascita').first()
+    giornalista_anziano=Giornalista.objects.order_by('anno_di_nascita').first()
 
     #13 gli ultmi 5 articoli pubblicati:
     ultimi= Articolo.objects.order_by('-data')[:5]
@@ -88,25 +89,25 @@ def query_base(request):
     articoli_minime_visualizzazioni= Articolo.objects.filter(visualizzazioni__gte=100)
 
      #15 tutti gli articoli che contengono una certa parola nel titolo:
-    articoli_parola= Articolo.objects.filter(titolo__icontains='importance')
+    articoli_parola= Articolo.objects.filter(titolo__icontains='importante')
 
     #creare il dizionario context
     context= {
         'articoli_cognome': articoli_cognome,
         'numero_totale_articoli': numero_totale_articoli,
-        'giornalista_1': giornalista_1,
         'numero_articoli_giornalista_1': numero_articoli_giornalista_1,
         'articoli_ordinati': articoli_ordinati,
-        'articoli_senza_ordinati': articoli_senza_ordinati,
+        'articoli_senza_visualizzazioni': articoli_senza_visualizzazioni,
         'articolo_piu_visualizzato': articolo_piu_visualizzato,
-        'gionalisti_data': gionalisti_data,
+        'giornalista_data': giornalista_data,
         'articoli_del_giorno': articoli_del_giorno,
         'articoli_periodo': articoli_periodo,
-        'gionalista_giovane': gionalista_giovane,
-        'gionalista_anziano': gionalista_anziano,
+        'articoli_giornalisti':articoli_giornalista,
+        'giornalista_giovane': giornalista_giovane,
+        'giornalista_anziano': giornalista_anziano,
         'ultimi': ultimi,
         'articoli_minime_visualizzazioni': articoli_minime_visualizzazioni,
         'articoli_parola': articoli_parola,
     }
 
-    return render (request, 'query.html', context)
+    return render (request, 'query_base.html', context)
