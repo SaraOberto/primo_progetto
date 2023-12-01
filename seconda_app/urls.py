7afab1ccf2a00a4d9fc152c955adf0db5638132a
SaraOberto <117723108+SaraOberto@users.noreply.github.com>
2023-12-01 08:25:04 +0100

    aggiornamenti

--- seconda_app/urls.py
+++ seconda_app/urls.py
@@ -4,9 +4,9 @@ from seconda_app.views import *
 
 app_name = "seconda_app"
 urlpatterns = [
-    path('/index_2', index_2, name = 'index_2'),
-    path('/es_if', es_if, name = 'es_if'),
-    path('/if_else_elif', if_else_elif, name = 'if_else_elif'),
-    path('/es_for', es_for, name = 'es_for'),
+    path('index_2', index_2, name = 'index_2'),
+    path('es_if', es_if, name = 'es_if'),
+    path('if_else_elif', if_else_elif, name = 'if_else_elif'),
+    path('es_for', es_for, name = 'es_for'),
 
 ]
